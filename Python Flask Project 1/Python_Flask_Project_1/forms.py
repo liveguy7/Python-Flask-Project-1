98ea8b9fcd2390c232a61e9653bd9990440713c3
jerom <jerom@LAPTOP-L0BU5SNG>
2024-01-15 15:29:42 -0500

    mod3

---
+++ Python Flask Project 1/Python_Flask_Project_1/forms.py
@@ -0,0 +1,10 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField
+from wtforms.validators import DataRequired, Length
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('UserName', 
+                           validators=[DataRequired(), Length(min=2, max=20)])
+
+
